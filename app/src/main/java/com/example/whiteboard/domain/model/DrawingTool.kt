d0dd813d7669ac2e566f4b52d3d647d399b900d6
sagar <sagaryadav12369@gmail.com>
2024-10-17 15:10:14 +0530

    new

---
+++ app/src/main/java/com/example/whiteboard/domain/model/DrawingTool.kt
@@ -0,0 +1,20 @@
+package com.example.whiteboard.domain.model
+
+import com.example.whiteboard.R
+
+enum class DrawingTool(
+    val resId: Int,
+) {
+    PEN(resId = R.drawable.img_pen),
+    HIGHLIGHTER(resId = R.drawable.img_highlighter),
+    LASER(resId = R.drawable.img_laser_pen),
+    ERASER(resId = R.drawable.img_eraser),
+    LINE_PLAIN(resId = R.drawable.ic_line_plain),
+    LINE_DOTTED(resId = R.drawable.ic_line_dotted),
+    ARROW_ONE_SIDED(resId = R.drawable.ic_arrow_one_sided),
+    ARROW_TWO_SIDED(resId = R.drawable.ic_arrow_one_sided),
+    RECTANGLE_FILLED(resId = R.drawable.ic_rectangle_filled),
+    CIRCLE_OUTSIDE(resId = R.drawable.ic_circle_outline),
+    CIRCLE_FILLED(resId = R.drawable.ic_circle_filled),
+    TRIANGLE_OUTLINE(resId = R.drawable.ic_triangle_outline),
+}
